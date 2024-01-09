85fc95c07d0ff01cbcbd39b366378d81d202045b
jamtechdev <jamtechba@gmail.com>
2024-01-09 11:56:20 +0530

    Bredcrum issue resolve

--- src/app/_components/ProductPage.jsx
+++ src/app/_components/ProductPage.jsx
@@ -2,7 +2,6 @@ import { GetCompareId } from "@/components/Product/GetCompareId.jsx";
 import React, { Fragment } from "react";
 import {
   Row,
-  Breadcrumb,
   Button,
   Col,
   Container,
@@ -17,7 +16,7 @@ import Link from "next/link";
 // import Compare from "../../components/Common/Compare/Compare";
 import ReviewSlider from "../../components/Common/ReviewSlider/reviewSlider";
 // import ComparisonsSlider from "../../components/Common/ComparisonsSlider/comparisonsSlider";
-// import BreadCrumb from "../../components/Common/BreadCrumb/breadcrum";
+import BreadCrumb from "@/components/Common/BreadCrumb/breadcrum";
 import MobileAccordion from "../../components/Common/MobileAccordion/mobileAccordion";
 // import ProductSlider from "../../components/Common/ProductSlider/productSlider";
 // import ProductCompareTable from "../../components/Common/CompareTable/ProductCompareTable";
@@ -161,7 +160,7 @@ function ProductPage({ productData }) {
         <Container>
           <Row className="align-items-center">
             <Col md={12}>
-              <Breadcrumb
+              <BreadCrumb
                 firstPageName="Electronics"
                 secondPageName={product?.name}
               />
