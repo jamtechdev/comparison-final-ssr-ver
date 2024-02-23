9f53e21d9b01d7414bb43ffb13cd5adcdd707855
Jamtech Dev <100343101+mohit-nigam77@users.noreply.github.com>
2024-02-23 17:46:36 +0530

    new changes

--- src/components/Common/OutlineGenerator/OutlineGenerator.jsx
+++ src/components/Common/OutlineGenerator/OutlineGenerator.jsx
@@ -6,48 +6,54 @@ function OutlineGenerator({ blogData }) {
   const [outline, setOutline] = useState([]);
   const [activeIndex, setActiveIndex] = useState(0);
   const [activeChildIndex, setActiveChildIndex] = useState(null);
+  const [activeChildSubChildIndex, setActiveChildSubChildIndex] =
+    useState(null);
 
   useEffect(() => {
-    const container = document.getElementById("shortCodeText");
-    const headings = container.querySelectorAll("h2, h3, h4");
-
+    // get all h2,h3,h4,h5,h6 elements
+    const shortCodeTextElement = document.getElementById("shortCodeText");
+    const headings = shortCodeTextElement.querySelectorAll("h2,h3,h4,h5");
     const newOutline = [];
-    let mainIndex = -1;
-    let subIndex = -1;
+
+    let currentMain = null;
+    let currentSubMain = null;
+    let currentSubSubMain = null;
 
     headings.forEach((heading) => {
       const id = heading.getAttribute("id");
       const dataId = heading.getAttribute("data-id");
 
       if (heading.tagName === "H2") {
-        newOutline.push({
+        currentMain = {
           type: "main",
           text: heading.textContent,
           id,
           children: [],
-        });
-        mainIndex++;
-        subIndex = -1;
-      } else if (heading.tagName === "H3" && mainIndex >= 0) {
-        newOutline[mainIndex].children.push({
+        };
+        newOutline.push(currentMain);
+        currentSubMain = null;
+        currentSubSubMain = null;
+      } else if (heading.tagName === "H3" && currentMain) {
+        currentSubMain = {
           type: "sub-main",
           text: heading.textContent,
           id,
           children: [],
-        });
-        subIndex++;
-      } else if (heading.tagName === "H4" && mainIndex >= 0 && subIndex >= 0) {
-        newOutline[mainIndex].children[subIndex].children.push({
+        };
+        currentMain.children.push(currentSubMain);
+        currentSubSubMain = null;
+      } else if (heading.tagName === "H4" && currentSubMain) {
+        currentSubSubMain = {
           type: "sub-sub-main",
           text: heading.textContent,
           id,
-        });
+        };
+        currentSubMain.children.push(currentSubSubMain);
       }
     });
 
     setOutline(newOutline);
   }, []);
-
   const handleScrollTo = (id) => {
     const element = document.getElementById(id);
     if (element) {
@@ -55,11 +61,14 @@ function OutlineGenerator({ blogData }) {
     }
   };
 
-  const handleItemClick = (index, childIndex, id) => {
+  const handleItemClick = (index, childIndex, childSubChildIndex, id) => {
     setActiveIndex(index);
     setActiveChildIndex(childIndex);
+    setActiveChildSubChildIndex(childSubChildIndex);
     handleScrollTo(id);
   };
+  console.log(outline);
+
   return (
     <>
       <ol>
@@ -84,35 +93,80 @@ function OutlineGenerator({ blogData }) {
             >
               {section.text}
             </Link>
-            <ol>
-              {section.children.map((child, childIndex) => (
-                <li
-                  key={childIndex}
-                  className={`outlineList ${
-                    activeChildIndex === childIndex && activeIndex === index
-                      ? "outline-active"
-                      : ""
-                  }`}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation(); // Stop event propagation here
-                    setActiveChildIndex(childIndex); // Update activeChildIndex here
-                    handleItemClick(index, childIndex, child.id);
-                  }}
-                >
-                  <Link
-                    href={`#${child?.id}`}
-                    className={`outlineLink ${
+            {section.children && (
+              <ol>
+                {section.children.map((child, childIndex) => (
+                  <li
+                    key={childIndex}
+                    className={`outlineList ${
                       activeChildIndex === childIndex && activeIndex === index
                         ? "outline-active"
                         : ""
                     }`}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation(); // Stop event propagation here
+                      setActiveChildIndex(childIndex); // Update activeChildIndex here
+                      handleItemClick(index, childIndex, "", child.id);
+                    }}
                   >
-                    {child.text}
-                  </Link>
-                </li>
-              ))}
-            </ol>
+                    <Link
+                      href={`#${child?.id}`}
+                      className={`outlineLink ${
+                        activeChildIndex === childIndex && activeIndex === index
+                          ? "outline-active"
+                          : ""
+                      }`}
+                    >
+                      {child.text}
+                    </Link>
+                    {child?.children && (
+                      <ol>
+                        {child?.children &&
+                          child?.children?.map((subSubMain, subChildIndex) => {
+                            return (
+                              <li
+                                key={subChildIndex}
+                                className={`outlineList ${
+                                  activeChildSubChildIndex === subChildIndex &&
+                                  activeChildIndex === childIndex
+                                    ? "outline-active"
+                                    : ""
+                                }`}
+                                onClick={(e) => {
+                                  e.preventDefault();
+                                  e.stopPropagation(); // Stop event propagation here
+                                  setActiveChildSubChildIndex(subChildIndex); // Update activeChildIndex here
+                                  handleItemClick(
+                                    index,
+                                    childIndex,
+                                    subChildIndex,
+                                    child.id
+                                  );
+                                }}
+                              >
+                                <Link
+                                  href={`#${child?.id}`}
+                                  className={`outlineLink ${
+                                    activeChildSubChildIndex ===
+                                      subChildIndex &&
+                                    activeChildIndex === childIndex
+                                      ? "outline-active"
+                                      : ""
+                                  }`}
+                                >
+                                  {subSubMain.text}
+                                </Link>
+                              </li>
+                            );
+                            //  {console.log(subSubMain?.text)}
+                          })}
+                      </ol>
+                    )}
+                  </li>
+                ))}
+              </ol>
+            )}
           </li>
         ))}
       </ol>
