582601a1692eba57e6f2bb2d1409e248d5436edb
Jamtech Dev <100343101+mohit-nigam77@users.noreply.github.com>
2024-03-07 19:27:56 +0530

    new changes

--- src/components/Common/Rating/Rating.jsx
+++ src/components/Common/Rating/Rating.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-
 const Rating = ({ value }) => {
   const stars = [];
   for (let i = 0; i < 5; i++) {
     if (i < Math.floor(value)) {
       stars.push(<i key={i} className="ri-star-fill"></i>);
-    } else if (i === Math.floor(value) && value % 1 !== 0) {
+    } else if (i === Math.floor(value) && value % 1 >= 0.5) {
       stars.push(<i key={i} className="ri-star-half-fill"></i>);
     } else {
       stars.push(<i key={i} className="ri-star-line"></i>);
