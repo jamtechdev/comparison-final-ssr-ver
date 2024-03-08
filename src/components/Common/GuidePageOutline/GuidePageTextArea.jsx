2671707f626f97ab9306af08c0c9e8e8c4852e79
Jamtech Dev <100343101+mohit-nigam77@users.noreply.github.com>
2024-03-08 19:36:21 +0530

    new changes

--- src/components/Common/GuidePageOutline/GuidePageTextArea.jsx
+++ src/components/Common/GuidePageOutline/GuidePageTextArea.jsx
@@ -11,7 +11,7 @@ function GuidePageTextArea({ guide }) {
   // This code for text part and outline
   useEffect(() => {
     const handleScroll = () => {
-      const headings = contentRef.current.querySelectorAll(
+      const headings = contentRef.current?.querySelectorAll(
         "h1, h2, h3, h4, h5, h6"
       );
 
